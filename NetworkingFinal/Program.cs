6088920eb65fadb23c4d478caceae77539d9a875
bwilson09 <bwilson09@mynbcc.ca>
2026-04-12 15:34:49 -0300

    changing where new accounts are saved to

--- NetworkingFinal/Program.cs
+++ NetworkingFinal/Program.cs
@@ -8,7 +8,10 @@ namespace NetworkingFinal
 {
     public class BankServer
     {
-        private static string path = Path.Combine(AppContext.BaseDirectory, "AppData", "accountStarter.json");
+        private static string path = Path.Combine(
+    Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName,
+    "AppData",
+    "accountStarter.json");
 
         public static List<Account> Accounts = new List<Account>();
         public static int Main(string[] args)
