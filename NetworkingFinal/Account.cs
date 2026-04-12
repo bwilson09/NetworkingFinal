1668cb0b5628edaf6ecc6d5b634a7818a2155cd1
Brittany Wilcox <bwilcox03@mynbcc.ca>
2026-04-12 10:42:32 -0300

    reorganizing project

---
+++ NetworkingFinal/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string Password { get; set; }
+        public string ReferenceNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
